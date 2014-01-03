428111a2391c828c7ca8093c864adbadc9b1940c
unknown <iftime_alexxandru@yahoo.com>
2014-01-03 17:06:37 +0200

    dsdasd

---
+++ elements/header_elem.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1,maximum-scale=1,user-scalable=1">  
+        <meta charset="UTF-8">
+        <!-- Site Header Content //-->
+        
+        <link rel="stylesheet" media="screen" type="text/css" href="/css/typography.css" />
+        <link rel="stylesheet" media="screen" type="text/css" href="/css/main.css" />
+        <link rel="stylesheet" media="screen" type="text/css" href="/css/elements.css" />
+        <link rel="stylesheet" media="screen" type="text/css" href="three_columns.css" />
+        <link rel="stylesheet" media="screen" type="text/css" href="/css/products.css" />
+        
+        <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="/js/verge.min.js"></script>
+        <script type="text/javascript">
+            jQuery.extend(verge);
+        </script>
+        <script src="/js/general.js"></script>
+        <script>
+            updateViewport() ;
+        </script>
+    </head>
+    <body>
+        <header class="goudas_header">
+            <section class="top_navigation">
+                <div class="container">
+
+                    <a href="javascript:void(0)" id="nav_btn">Menu</a>
+
+                    <div class="row1">
+                        <div class="logo span2h">
+                            <a href="/" target="_self"><img src="/images/logo.png"></a>
+                        </div>
+
+                        <div class="span10h">
+
+                            <div class="row1 header_row_1">
+                                <div class="span">
+                                    <div class="header_content">
+                                        <p>Website voor professionele gebruikers in de horeca, Gouda’s Glorie Foodservice!</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row1">
+                                <nav class="span pr">
+                                    
+                                    <!--TOP MENU MENU-->
+                                    
+                                </nav>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </header>
+
