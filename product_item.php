2814efbcbcf898f79a461aaac49cfba3adba9bba
Gurzun Ilie <octawyan12@yahoo.com>
2014-01-03 20:34:51 +0200

    fix

--- product_item.php
+++ product_item.php
@@ -8,6 +8,14 @@ if(isset($_GET['id'])) {
 } else {
     $sql = mysql_query("SELECT * FROM product");
     while ($products[] = mysql_fetch_assoc($sql));
-    var_dump($products);die;
+    foreach($products as $key=>$product) {
+        if($product['image'] != 0) {
+            $img = mysql_query('Select * from image where id='.intval($product['image']));
+            $img = mysql_fetch_assoc($img);
+            $img = "/upload/".$img['name'];
+            $product['image'] = $img;
+            $products[$key] = $product;
+        }
+    }
 }
 ?>
