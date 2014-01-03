7eac6f8cb23fd0d0019f214fc688d5f810ab8a98
Gurzun Ilie <octawyan12@yahoo.com>
2014-01-03 20:10:14 +0200

    dump

--- site.sql
+++ site.sql
@@ -23,10 +23,15 @@ DROP TABLE IF EXISTS `image`;
 CREATE TABLE `image` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `extension` varchar(255) NOT NULL,
+  `size` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 /*Data for the table `image` */
+
+insert  into `image`(`id`,`name`,`type`,`extension`,`size`) values (5,'prt_scr.png','image/png','png',365.306),(6,'prt_scr.png','image/png','png',365.306),(7,'prt_scr.png','image/png','png',365.306),(8,'prt_scr.png','image/png','png',365.306),(9,'prt_scr.png','image/png','png',365.306),(10,'pre_beach2aw.jpg','image/jpeg','jpg',26.3633),(11,'pre_beach2aw.jpg','image/jpeg','jpg',26.3633),(12,'243tenebra (35).jpg','image/jpeg','jpg',1747.23),(13,'243tenebra (35).jpg','image/jpeg','jpg',1747.23),(14,'prt_scr.png','image/png','png',365.306);
 
 /*Table structure for table `menu` */
 
@@ -53,11 +58,11 @@ CREATE TABLE `menu_item` (
   `menu` int(11) NOT NULL,
   `require_login` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`,`menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `menu_item` */
 
-insert  into `menu_item`(`id`,`title`,`path`,`menu`,`require_login`) values (1,'Home','/index.php',1,1),(2,'Contact','/contact.php',1,0);
+insert  into `menu_item`(`id`,`title`,`path`,`menu`,`require_login`) values (1,'Home','/index.php',1,1),(2,'Contact','/contact.php',1,0),(3,'Add Product','/add-new.php',1,1);
 
 /*Table structure for table `product` */
 
@@ -70,9 +75,11 @@ CREATE TABLE `product` (
   `image` int(11) DEFAULT NULL,
   `price` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `product` */
+
+insert  into `product`(`name`,`id`,`description`,`image`,`price`) values ('qweqwe',1,'qweqwe',14,2313),('sadasd',2,'asdasd',0,2313),('sadasd',3,'asdasd',0,2313),('sadasd',4,'asdasd',0,2313),('asdasd',5,'asdasd',0,324234),('asdasd',6,'asdasd',0,324234);
 
 /*Table structure for table `user` */
 
